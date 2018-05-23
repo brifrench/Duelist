d352625c82409bda2b8ed83eaba09ee540c3f768
brifrench <bri.r.french@gmail.com>
2018-05-23 18:31:49 -0400

    Inital Creation

---
+++ Assets/Movement.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    GameObject PlayerArea;
+    [SerializeField]
+    GameObject[] spells;
+
+
+    public int speed = 1;
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+    
+
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    private double squareLength = 4.5;
+
+
+    public GameObject projectile;
+    public Transform Spawnpoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+
+    void Update()
+    {
+
+
+        //if (Input.GetMouseButton(1) == true)
+        {
+            yaw += speedH * Input.GetAxis("Mouse X");
+            pitch -= speedV * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+
+        if (Input.GetMouseButtonDown(1) == true)
+        {
+
+
+  
+            GameObject bullet = Instantiate(spells[2], Spawnpoint.position, this.transform.rotation );
+            //Rigidbody holder = bullet.AddComponent<Rigidbody>();
+            
+            //holder.mass = .1f;
+            //holder.collisionDetectionMode = CollisionDetectionMode.Continuous;
+            //holder.AddForce(transform.forward * 100);
+            //holder.useGravity = false;
+
+
+          
+
+        }
+
+
+        Vector3 pos = transform.position;
+
+
+
+        if (Input.GetKey("w"))
+        {
+            if (pos.z + speed * Time.deltaTime <= squareLength)
+            {
+                pos.z += speed * Time.deltaTime;
+
+            }
+
+        }
+        if (Input.GetKey("s"))
+        {
+            if (pos.z - speed * Time.deltaTime >= -squareLength)
+            {
+                pos.z -= speed * Time.deltaTime;
+
+            }
+
+        }
+        if (Input.GetKey("d"))
+        {
+            if (pos.x + speed * Time.deltaTime <= squareLength)
+            {
+                pos.x += speed * Time.deltaTime;
+
+            }
+
+        }
+        if (Input.GetKey("a"))
+        {
+            if (pos.x - speed * Time.deltaTime >= -squareLength)
+            {
+                pos.x -= speed * Time.deltaTime;
+            }
+
+        }
+
+
+        transform.position = pos;
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public int health = 100;
+
+    
+
+
+    private Hand[] left;
+    private Hand[] right;
+
+
+	// Use this for initialization
+	void Start () {
+        left = new Hand[2];
+        right = new Hand[2];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health < 0)
+        {
+            destroyPlayer();
+        }
+    }
+
+    public void dealDamage(int dmg)
+    {
+        health -= dmg;
+        if (health < 0)
+        {
+            destroyPlayer();
+        }
+    }
+
+    private void destroyPlayer()
+    {
+        //TODO remove player or end match
+    }
+}
+
+
+public class Hand
+{
+    Spell[] left;
+    Spell[] right;
+
+    public Hand()
+    {
+        Spell[] left = new Spell[4];
+        Spell[] right = new Spell[4];
+    }
+
+
+    public void createSpell(int element, int type, int location, int hand)
+    {
+        Spell temp;
+        if( type == 0)
+        {
+            temp = new Shield(element);
+        }
+        else
+        {
+            temp = new Attack(element);
+        }
+
+        
+        if(hand == 1)
+        {
+            left[location--] = temp;
+        }
+        else
+        {
+            right[location--] = temp;
+        }
+
+
+    }
+}
+
+public class Spell
+{
+    public int SpellElement;
+    public int damage;
+    
+    public Spell()
+    {
+
+    }
+
+    public Spell(int element, int type)
+    {
+        ///Element
+        ///0 Neutral
+        ///1 Fire
+        ///2 Water
+        ///3 Lightning
+        ///4 Earth
+        ///Type
+        ///1 Attack
+        ///2 Shield
+        ///
+
+        SpellElement = element;
+
+        switch (element)
+        {
+            case 0:
+                damage = 50;
+                break;
+            case 1:
+                damage = 75;
+                break;
+            case 2:
+                damage = 100;
+                break;
+            case 3:
+                damage = 100;
+                break;
+            case 4:
+                damage = 75;
+                break;
+            
+        }
+
+        switch (type)
+        {
+            case 2:
+                damage *= -1;
+                break;
+            
+        }
+        
+        
+        
+
+    }
+}
+
+public class Shield : Spell
+{
+    public Shield()
+    {
+
+    }
+
+    public Shield(int element)
+    {
+        SpellElement = element;
+        switch (element)
+        {
+            case 0:
+                damage = 50;
+                break;
+            case 1:
+                damage = 75;
+                break;
+            case 2:
+                damage = 100;
+                break;
+            case 3:
+                damage = 100;
+                break;
+            case 4:
+                damage = 75;
+                break;
+
+        }
+
+        damage *= -1;
+    }
+}
+
+public class Attack : Spell
+{
+
+    public Attack()
+    {
+
+    }
+
+    public Attack(int element)
+    {
+        SpellElement = element;
+        switch (element)
+        {
+            case 0:
+                damage = 50;
+                break;
+            case 1:
+                damage = 75;
+                break;
+            case 2:
+                damage = 100;
+                break;
+            case 3:
+                damage = 100;
+                break;
+            case 4:
+                damage = 75;
+                break;
+
+        }
+    }
+}
+
+
